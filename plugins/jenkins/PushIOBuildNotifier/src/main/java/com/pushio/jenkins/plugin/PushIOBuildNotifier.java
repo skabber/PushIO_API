ca04bfd1251d32e2f881dab5ff4c61161bfe7879
Jay Graves <jay@doubleencore.com>
2012-08-09 17:14:47 -0600

    Create a PushIO notification plugin for Jenkins.

---
+++ plugins/jenkins/PushIOBuildNotifier/src/main/java/com/pushio/jenkins/plugin/PushIOBuildNotifier.java
@@ -0,0 +1,107 @@
+package com.pushio.jenkins.plugin;
+
+import hudson.EnvVars;
+import hudson.Extension;
+import hudson.Launcher;
+import hudson.model.*;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.BuildStepMonitor;
+import hudson.tasks.Notifier;
+import hudson.tasks.Publisher;
+import java.lang.String;
+import java.lang.InterruptedException;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jay Graves
+ * Date: 7/29/12
+ * Time: 1:18 PM
+ */
+public class PushIOBuildNotifier extends Notifier {
+
+    private final String appID;
+    private final String serviceSecret;
+    private final String pushCategory;
+    private final String payload;
+    private final static Logger LOG = Logger.getLogger(PushIOBuildNotifier.class.getName());
+
+    @DataBoundConstructor
+    public PushIOBuildNotifier(String appID, String serviceSecret, String pushCategory, String payload) {
+        super();
+
+        this.appID = appID;
+        this.serviceSecret = serviceSecret;
+        this.pushCategory = pushCategory;
+        this.payload = payload;
+    }
+
+    public String getAppID() {
+        return appID;
+    }
+
+    public String getServiceSecret() {
+        return serviceSecret;
+    }
+
+    public String getPushCategory() {
+        return pushCategory;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+
+    @Override
+    public BuildStepMonitor getRequiredMonitorService() {
+        return BuildStepMonitor.BUILD;
+    }
+
+    @Override
+    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
+        EnvVars vars = build.getEnvironment(listener);
+
+        if (build.getResult().isWorseOrEqualTo(Result.FAILURE))
+            return false;
+
+        HttpClient client = new HttpClient();
+
+        String url = "https://manage.push.io/api/v1/notify_app/"+vars.expand(appID)+"/"+vars.expand(serviceSecret);
+
+        PostMethod method = new PostMethod(url);
+        method.setParameter("payload", vars.expand(payload));
+        method.setParameter("tag_query", vars.expand(pushCategory));
+
+        client.executeMethod(method);
+
+        String response = method.getResponseBodyAsString();
+        System.out.println(response);
+        method.releaseConnection();
+
+        return true;
+    }
+
+    @Extension
+    public static class DescriptorImpl extends BuildStepDescriptor<Publisher> {
+        public DescriptorImpl() {
+            load();
+        }
+
+        @Override
+        public String getDisplayName(){
+            return "PushIO Notification";
+        }
+
+        @Override
+        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+            return true;
+        }
+    }
+}
